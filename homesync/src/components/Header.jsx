4d0be893c3ec712de3d779a5c03b4cd132d4c5b2
Zhile Lin <zhlin04@gmail.com>
2023-03-14 01:40:27 -0700

    sidebar style initial

--- homesync/src/components/Header.jsx
+++ homesync/src/components/Header.jsx
@@ -37,6 +37,7 @@ const Container = styled.div`
         align-items:center;
         justify-content:center;
         position:relative;
+        box-shadow: 0 1px 0 0 rgb(255 255 255 /10%);
         `
 
 const Main = styled.div`
--- homesync/src/components/Sidebar.jsx
+++ homesync/src/components/Sidebar.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 
 function Sidebar() {
   return (
-    <div>
-       Sidebar
-    </div>
+    <Container>
+      <WorkspaceContainer>
+          <Name>
+           App Academy
+          </Name>
+          <NewMessage>
+               <BorderColorOutlinedIcon/>
+          </NewMessage>
+      </WorkspaceContainer>
+    </Container>
   )
 }
 
 export default Sidebar
+
+
+const Container = styled.div`
+     background: #3F0E40`
+
+const WorkspaceContainer = styled.div`
+    color:white;
+    height:64px;
+    display:flex;
+    align-items:center;
+    padding-left:19px;
+    justify-content: space-between;
+    border-bottom: 1px solid #532753;
+    
+ `
+
+const Name = styled.div`
+ `
+
+const NewMessage = styled.div`
+     width:36px;
+     height:36px;
+     background:white;
+     color:#3F0E40;
+     fill:#3F0E40;
+     display:flex;
+     justify-content:center;
+     align-items: center;
+     border-radius: 50%;
+     margin-right:20px;
+     cursor:pointer;
+ `
