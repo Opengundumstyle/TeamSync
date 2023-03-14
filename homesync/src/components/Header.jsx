35a903954dfac0d0ba40a53960e006c890730db1
Zhile Lin <zhlin04@gmail.com>
2023-03-13 23:25:03 -0700

    css for header

--- homesync/src/components/Header.jsx
+++ homesync/src/components/Header.jsx
@@ -39,12 +39,29 @@ const Container = styled.div`
         `
 
 const Main = styled.div`
-       display:flex
+       display:flex;
        `
 
-const SearchContariner = styled.div``
+const SearchContariner = styled.div`
+      min-width:400px;`
 
-const Search = styled.div``
+const Search = styled.div`
+     box-shadow: inset 0 0 0 1px rbg(104 74 104);
+     width:100%;
+     border-radius:6px;
+
+     input{
+         background-color: transparent;
+         border:none;
+         padding-left:8px;
+         padding-right:8px;
+         color:white;
+      }
+      
+      input:focus{
+         outline:none;
+      }
+      `
 
 
 const UserContainer = styled.div`
