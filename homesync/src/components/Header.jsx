f6f73d59b4de1848a5bc08d08dcd1b96e2ea5852
Zhile Lin <zhlin04@gmail.com>
2023-03-14 00:28:38 -0700

    final touch with header

--- homesync/src/components/Header.jsx
+++ homesync/src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
                   Brian
               </Name>
               <UserImage>
-                   <img src="https://i.pinimg.com/564x/71/94/58/71945842e6985f179772c11a65bfb3c9.jpg" width="30px" />
+                   <img src="https://i.pinimg.com/564x/71/94/58/71945842e6985f179772c11a65bfb3c9.jpg" />
               </UserImage>
         </UserContainer>
     </Container>
@@ -35,27 +35,35 @@ const Container = styled.div`
         color:white;
         display:flex;
         align-items:center;
-        justify-content:space-between;
+        justify-content:center;
+        position:relative;
         `
 
 const Main = styled.div`
        display:flex;
+       margin-right:16px;
+       margin-left:16px;
        `
 
 const SearchContariner = styled.div`
-      min-width:400px;`
+      min-width:400px;
+      margin-left:16px;
+      margin-right:16px;`
 
 const Search = styled.div`
      box-shadow: inset 0 0 0 1px rbg(104 74 104);
      width:100%;
      border-radius:6px;
-
+     display:flex;
+     align-items:center;
      input{
          background-color: transparent;
          border:none;
          padding-left:8px;
          padding-right:8px;
          color:white;
+         padding-top:4px;
+         padding-bottom:4px;
       }
       
       input:focus{
@@ -66,9 +74,21 @@ const Search = styled.div`
 
 const UserContainer = styled.div`
       display:flex;
-      align-items:center;`
+      align-items:center;
+      padding-right: 16px;
+      position:absolute;
+      right:0;
+      `
 
-const Name = styled.div``
+const Name = styled.div`
+     padding-right:16px;`
 
 const UserImage = styled.div`
+           width: 28px;
+           height:28px;
+           border: 2px solid white;
+           border-radius:3px;
+           img{
+             width:100%;
+           }
        `
